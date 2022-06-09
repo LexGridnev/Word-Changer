eb2c6376fed6daca797654d0b7c4dc62b1eb4015
LexGridnev <lex.gridnev@gmail.com>
2022-06-09 21:23:53 +0300

    no need frame

--- prog_gui.py
+++ prog_gui.py
@@ -3,7 +3,6 @@
 import csv
 import tkinter as tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
-from tkinter import Frame
 
 #Get text from Editor
 def text_input():
@@ -17,9 +16,9 @@ def text_edit():
     txt_edit.configure(state='normal')
     text = text_input()
     file_csv = [i.split(',') for i in csv_edit.get("1.0",tk.END).strip('\n').split("\n", 2)]
-    print(file_csv)
+    #print(file_csv)
     file_dictionary = dict(file_csv)
-    print(file_dictionary)
+    #print(file_dictionary)
     for i in file_dictionary:
         text = text.replace(i,file_dictionary[i])
     txt_edit.delete('1.0', tk.END)
@@ -60,26 +59,6 @@ def open_csv():
         csv_edit.insert(tk.END, text)
 
 
-#############################3
-'''
-new_path = open("mytest.csv", "w")
-
-file_dictionary = {
-              "Hui":"Vasiliy",
-              "pizda":"Nadezda",
-              "poezda":"prjatki",
-                                 }
-
-z = csv.writer(new_path)
-for new_k, new_v in file_dictionary.items():
-    z.writerow([new_k, new_v])
-
-new_path.close()
-
-
-'''
-##############################
-
 def save_file():
     """Save the current file as a new file."""
     filepath = asksaveasfilename(
@@ -95,25 +74,24 @@ def save_file():
 
 window = tk.Tk()
 window.title("Simple Words Replacer")
-window.rowconfigure(0, minsize=800, weight=0)
-window.columnconfigure(1, minsize=400, weight=0)
-
-csv_frame = tk.Frame(window)
-csv_frame.columnconfigure(0, minsize=50, weight=0)
+window.rowconfigure(0, minsize=100, weight=0)
+window.columnconfigure(0, minsize=100, weight=0)
 
 txt_edit = tk.Text(window)
 csv_edit = tk.Text(window)
+
 frm_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
 csv_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
+
 btn_open = tk.Button(frm_buttons, text="Load Text", command = open_file)
 btn_csv =  tk.Button(csv_buttons, text="Load CSV", command = open_csv)
 btn_repl = tk.Button(frm_buttons, text="Replace words", command = text_edit)
 btn_save = tk.Button(frm_buttons, text="Save As ...", command = save_file)
 
-btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
-btn_csv.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
-btn_repl.grid(row=2, column=0, sticky="ew", padx=5, pady=5)
-btn_save.grid(row=3, column=0, sticky="ew", padx=5, pady=5)
+btn_open.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
+btn_csv.grid(row=1, column=0, sticky="nsew", padx=5, pady=5)
+btn_repl.grid(row=2, column=0, sticky="nsew", padx=5, pady=5)
+btn_save.grid(row=3, column=0, sticky="nsew", padx=5, pady=5)
 
 frm_buttons.grid(row=0, column=0, sticky="ns")
 csv_buttons.grid(row=0, column=3, sticky="ns")
@@ -121,7 +99,3 @@ txt_edit.grid(row=0, column=1, sticky="nsew")
 csv_edit.grid(row=0, column=2, sticky="nsew")
 
 window.mainloop()
-
-
-
-    
